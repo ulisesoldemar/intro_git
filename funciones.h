4b14ba50c403d939fe6f930a0afb87b0f88f61e1
Ulises Oldemar Ortega Gutierrez <ulises.ortega2937@alumnos.udg.mx>
2020-09-21 14:22:28 -0500

    Funciones suma() y resta() implementadas

--- funciones.h
+++ funciones.h
@@ -1,14 +1,14 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
-void suma(int a, int b) 
+int suma(int a, int b) 
 {
-
+    return a + b;
 }
 
-void resta(int a, int b)
+int resta(int a, int b)
 {
-    
+    return a - b;
 }
 
 #endif // FUNCIONES_H

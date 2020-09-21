83408da366caf70a221e81f2a5053f3e930a287a
Ulises Oldemar Ortega Gutierrez <ulises.ortega2937@alumnos.udg.mx>
2020-09-21 13:58:16 -0500

    Se comento el hola mundo

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main()
     char nombre[100];
     unsigned int edad;
 
-    printf("Hola mundo\n");
+    // printf("Hola mundo\n");
     printf("Nombre: ");
     fgets(nombre, sizeof(nombre), stdin);
     printf("Hola %s", nombre);
